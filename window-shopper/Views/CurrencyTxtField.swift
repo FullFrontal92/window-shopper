d7705cd74a77cd5ee2115e7b50c12377f7576972
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-02-19 16:57:21 +0100

    working on ui,group for views create with file for currency

---
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Adam Benyahia on 19/02/2018.
+//  Copyright © 2018 Adam Benyahia. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
